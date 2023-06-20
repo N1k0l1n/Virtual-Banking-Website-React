1479777565968fe32bd960b2b4fd4523e94b18e4
Nikolin Andoni <97979235+N1k0l1n@users.noreply.github.com>
2023-06-20 11:24:11 +0200

    adding next section = images

--- src/components/InfoSection/InfoElement.js
+++ src/components/InfoSection/InfoElement.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const InfoContainer = styled.div`
   color: #fff;
-  background: ${({ lightBg }) => (lightBg ? "#f9f9f9" : "010606")};
+  background: ${({ lightBg }) => (lightBg ? "#f9f9f9" : "#010606")};
 
   @media screen and (max-width: 768px) {
     padding: 100px 0;
@@ -25,18 +25,19 @@ export const InfoRow = styled.div`
   display: grid;
   grid-auto-columns: minmax(auto, 1fr);
   align-items: center;
-  /* grid-template-areas: ${({ imgStart }) =>
-    imgStart ? `'col1' 'col2'` : `'col1 col1' 'col2 col2'`}; */
-  /* 
+  grid-template-areas: ${({ imgStart }) =>
+    imgStart ? `'col1' 'col2'` : `'col1 col1' 'col2 col2'`}; 
+  
     @media screen and (max-width: 768px) {
-        
-    } */
+        grid-template-areas:${({ imgStart }) =>
+    imgStart ? `'col1' 'col2'` : `'col1 col1' 'col2 col2'`}; 
+    }
 `;
 
 export const Column1 = styled.div`
   margin-bottom: 15px;
   padding: 0 15px;
-  grid-area: col2;
+  grid-area: col1;
 `;
 
 export const Column2 = styled.div`
@@ -67,7 +68,7 @@ color: #000;
   font-size: 48px;
   line-height: 1.1;
   font-weight: 600;
-  /* color: ${({ lineText }) => (lineText ? "#f7f8fa" : "010606")}; */
+  color: ${({ lineText }) => (lineText ? "#f7f8fa" : "white")};
 
   @media screen and (max-width: 480px) {
     font-size: 32px;
@@ -79,7 +80,7 @@ export const Subtitle = styled.p`
   margin-bottom: 35px;
   font-size: 18px;
   line-height: 24px;
-  /* color: ${({ darkText }) => (darkText ? "#010606" : "#fff")}; */
+  color: ${({ darkText }) => (darkText ? "#010606" : "#fff")};
 `;
 
 export const ImgWrap = styled.div`
